7e52d73f661bf4dd9f5c6f2f584a0d42213ce67f
Saioa Saenz de Buruaga Diez <ssaenzdi31@alumnes.ub.edu>
2025-09-25 15:50:25 +0200

    Prova 1

--- src/Rental.java
+++ src/Rental.java
@@ -6,7 +6,29 @@ public class Rental
 		this.daysRented = daysRented;
 	}
 
-	public int getDaysRented () {
+    static double getRentalPrice(Rental rental) {
+        double thisAmount = 0;
+
+        // determines the amount for each line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    public int getDaysRented () {
 		return daysRented;
 	}
 
